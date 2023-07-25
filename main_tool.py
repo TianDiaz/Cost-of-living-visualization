2318608ca49a565f09c72f9ca55fdddb3c8f7290
Christian Diaz <110827614+TianDiaz@users.noreply.github.com>
2023-07-25 19:24:07 +0800

    Added more comments to easily understand some parts of the script, and added a feature that changes the color of the bar that has the minimum and maximum value

--- main_tool.py
+++ main_tool.py
@@ -5,34 +5,57 @@ from tkinter import filedialog
 
 
 def calculate_cost_of_living(csv_file):
+    # Data Collection
+    # The 'csv_file' is from the user
     data = pd.read_csv(csv_file)
 
+    # Check if the required columns exists in the DataFrame
     required_columns = ['Food', 'Housing', 'Clothes', 'Transportation', 'Personal Care', 'Entertainment']
     missing_columns = [col for col in required_columns if col not in data.columns]
     if missing_columns:
         raise KeyError(f"The following columns are missing in the DataFrame: {', '.join(missing_columns)}")
 
+    # Data Analysis
+    # Calculating the total cost of living by summing up the expenses in each category
     data['Total_Cost_of_Living'] = data[['Food', 'Housing', 'Clothes', 'Transportation', 'Personal Care', 'Entertainment']].sum(axis=1)
 
+    # Calculating the national average cost of living
     national_average = data['Total_Cost_of_Living'].mean()
     return data, national_average
 
 
 def plot_cost_of_living(data, national_average):
+    # Data Visualization
+    # Bar plot for comparing the total cost of living for each province
     plt.figure(figsize=(10, 6))
-    plt.bar(data['Province'], data['Total_Cost_of_Living'])
+    bars = plt.bar(data['Province'], data['Total_Cost_of_Living'])
     plt.axhline(y=national_average, color='r', linestyle='dashed', label='National Average')
-    plt.xlabel('Province')
+    plt.xlabel('Province/City')
     plt.ylabel('Total Cost of Living')
-    plt.title('Cost of Living Comparison by Province')
+    plt.title('Cost of Living Comparison by Province/City')
     plt.legend()
     plt.xticks(rotation=90)
 
-    # Adding the text annotation for the average value
+    # Text annotation for the average value
     plt.annotate(f'National Average: {national_average:.2f}',
                  xy=(0, national_average), xycoords='data',
                  xytext=(0, 35), textcoords='offset points',
                  arrowprops=dict(arrowstyle="->", color='r', linewidth=2))
+
+    # Changing the color  and displaying the value of the bar with max and min value
+    max_index = data['Total_Cost_of_Living'].idxmax()
+    min_index = data['Total_Cost_of_Living'].idxmin()
+    # #DA2C38
+    bars[max_index].set_color('#DA2C38')  # Set color for the bar with the maximum value
+    bars[min_index].set_color('g')  # Set color for the bar with the minimum value
+    plt.annotate(f'Max: {data["Total_Cost_of_Living"].max():.2f}',
+                 xy=(max_index, data['Total_Cost_of_Living'].max()), xycoords='data',
+                 xytext=(6, -4), textcoords='offset points', color='#DA2C38',
+                 ha='left', va='bottom', fontsize=12)
+    plt.annotate(f'Min: {data["Total_Cost_of_Living"].min():.2f}',
+                 xy=(min_index, data['Total_Cost_of_Living'].min()), xycoords='data',
+                 xytext=(10, -16), textcoords='offset points', color='white',
+                 ha='left', va='bottom', fontsize=12, bbox=dict(boxstyle="round,pad=0.3", fc="g", ec="white", lw=1))
 
     plt.tight_layout()
     plt.show()
@@ -44,11 +67,12 @@ def browse_file():
         data, national_average = calculate_cost_of_living(filepath)
         plot_cost_of_living(data, national_average)
 
-# Window configurations
+# Window for the application
 root = tk.Tk()
 root.title("Cost of Living Analysis")
 root.configure(bg='#D1D1D1')
 
+# Size of the main window
 window_width = 800
 window_height = 600
 screen_width = root.winfo_screenwidth()
@@ -57,13 +81,15 @@ x_coordinate = int((screen_width / 2) - (window_width / 2))
 y_coordinate = int((screen_height / 2) - (window_height / 2))
 root.geometry(f"{window_width}x{window_height}+{x_coordinate}+{y_coordinate}")
 
-label = tk.Label(root, text="Cost of Living", font=("Arial", 50), bg='#D1D1D1')
-label.pack(pady=10)
+# Label widget to display the "Cost of Living" text
+label = tk.Label(root, text="Cost of Living Analysis", font=("Arial", 50), bg='#D1D1D1')
+label.pack(pady=90)
 
+# Button to browse and select the CSV file
 button_width = 20
 button_height = 1
 upload_btn = tk.Button(root, text="Upload CSV File", command=browse_file, font=("Arial", 18), width=button_width, height=button_height, bg='#636363', fg='#faf9f6', cursor='hand2')
-upload_btn.place(relx=0.5, rely=0.5, anchor=tk.CENTER)  # Center the button
+upload_btn.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
 # Start the main event loop
 root.mainloop()
